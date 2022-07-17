13dbbf1605bcafc63e09092c52224676a669ac66
Pavel <p.storozhenko@tinkoff.ru>
2022-07-17 16:48:34 +0300

    first commit

---
+++ provision.sh
@@ -0,0 +1,13 @@
+set -eox
+# install oh-my-zsh
+curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -o install-oh-my-zsh.sh
+sh install-oh-my-zsh.sh
+rm install-oh-my-zsh.sh
+# install homebrew
+curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh -o homebrew.sh
+/bin/bash homebrew.sh
+rm homebrew.sh
+# install ansible
+brew install ansible
+# provision
+ansible-playbook playbook.yaml
